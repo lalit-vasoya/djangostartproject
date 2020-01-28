141d401c169c1076c9e96a82d6b6d19d42eca908
lalit-vasoya <lalitvasoya.286@gmail.com>
2020-01-28 11:27:43 +0530

    cbview change in sqllite data base

--- djmod/blog/views.py
+++ djmod/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView,TemplateResponseMixin,ContextMixin
 from django.views.generic import View
 from django.http import HttpResponse
-# Create your views here.
+# """Create your views here."""
 
 
 
